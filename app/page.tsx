0f802f37e25a572c6e91d65b81fcf125e4651e11
Akarshan Mishra <93630550+1pharaxh@users.noreply.github.com>
2024-01-20 10:09:50 -0700

    feat: added gantt

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,106 @@
-import Image from "next/image";
+"use client";
+import {
+  Gantt,
+  Task,
+  EventOption,
+  StylingOption,
+  ViewMode,
+  DisplayOption,
+} from "gantt-task-react";
+import "gantt-task-react/dist/index.css";
+const currentDate = new Date();
+
+const tasks: Task[] = [
+  {
+    start: new Date(currentDate.getFullYear(), currentDate.getMonth(), 1),
+    end: new Date(currentDate.getFullYear(), currentDate.getMonth(), 15),
+    name: "Some Project",
+    id: "ProjectSample",
+    progress: 25,
+    type: "project",
+    hideChildren: false,
+    displayOrder: 1,
+  },
+  {
+    start: new Date(currentDate.getFullYear(), currentDate.getMonth(), 1),
+    end: new Date(currentDate.getFullYear(), currentDate.getMonth(), 2, 12, 28),
+    name: "Idea",
+    id: "Task 0",
+    progress: 45,
+    type: "task",
+    project: "ProjectSample",
+    displayOrder: 2,
+  },
+  {
+    start: new Date(currentDate.getFullYear(), currentDate.getMonth(), 2),
+    end: new Date(currentDate.getFullYear(), currentDate.getMonth(), 4, 0, 0),
+    name: "Research",
+    id: "Task 1",
+    progress: 25,
+    dependencies: ["Task 0"],
+    type: "task",
+    project: "ProjectSample",
+    displayOrder: 3,
+  },
+  {
+    start: new Date(currentDate.getFullYear(), currentDate.getMonth(), 4),
+    end: new Date(currentDate.getFullYear(), currentDate.getMonth(), 8, 0, 0),
+    name: "Discussion with team",
+    id: "Task 2",
+    progress: 10,
+    dependencies: ["Task 1"],
+    type: "task",
+    project: "ProjectSample",
+    displayOrder: 4,
+  },
+  {
+    start: new Date(currentDate.getFullYear(), currentDate.getMonth(), 8),
+    end: new Date(currentDate.getFullYear(), currentDate.getMonth(), 9, 0, 0),
+    name: "Developing",
+    id: "Task 3",
+    progress: 2,
+    dependencies: ["Task 2"],
+    type: "task",
+    project: "ProjectSample",
+    displayOrder: 5,
+  },
+  {
+    start: new Date(currentDate.getFullYear(), currentDate.getMonth(), 8),
+    end: new Date(currentDate.getFullYear(), currentDate.getMonth(), 10),
+    name: "Review",
+    id: "Task 4",
+    type: "task",
+    progress: 70,
+    dependencies: ["Task 2"],
+    project: "ProjectSample",
+    displayOrder: 6,
+  },
+  {
+    start: new Date(currentDate.getFullYear(), currentDate.getMonth(), 15),
+    end: new Date(currentDate.getFullYear(), currentDate.getMonth(), 15),
+    name: "Release",
+    id: "Task 6",
+    progress: currentDate.getMonth(),
+    type: "milestone",
+    dependencies: ["Task 4"],
+    project: "ProjectSample",
+    displayOrder: 7,
+  },
+  {
+    start: new Date(currentDate.getFullYear(), currentDate.getMonth(), 18),
+    end: new Date(currentDate.getFullYear(), currentDate.getMonth(), 19),
+    name: "Party Time",
+    id: "Task 9",
+    progress: 0,
+    isDisabled: true,
+    type: "task",
+  },
+];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="p-10">
+      <Gantt tasks={tasks} columnWidth={100} listCellWidth="100px" />
     </main>
   );
 }
